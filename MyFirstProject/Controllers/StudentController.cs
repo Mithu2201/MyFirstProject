849b2739818647ad819cc699e37af3f6988b086a
Mithu2201 <mithu2201@gmail.com>
2025-07-12 12:50:39 +0530

    Add project files.

---
+++ MyFirstProject/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstProject.Data;
+using MyFirstProject.Models;
+
+namespace MyFirstProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDBContext dbContext;
+
+        public StudentController(ApplicationDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstProject/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MyFirstProject.Models;
+
+namespace MyFirstProject.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
